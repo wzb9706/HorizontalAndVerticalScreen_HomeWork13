b0916b77f2e6b653abda9545f0e9613cba3258e9
wzb9706 <wzb9706@sina.com>
2016-09-05 01:06:02 +0800

    Initial commit

---
+++ app/src/test/java/com/example/wzb97/horizontalandverticalscreen_homework13/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.wzb97.horizontalandverticalscreen_homework13;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
